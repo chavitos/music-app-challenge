f6f09ded1541aca2a78f9650b8fb0216c50dba87
Tiago Chaves <tchaves74@gmail.com>
2026-04-12 15:18:55 -0300

    Project creation and initial configuration

---
+++ myTunes/myTunesTests/myTunesTests.swift
@@ -0,0 +1,19 @@
+//
+//  myTunesTests.swift
+//  myTunesTests
+//
+//  Created by Tiago Chaves on 12/04/26.
+//
+
+import Testing
+@testable import myTunes
+
+struct myTunesTests {
+	
+	@Test func example() async throws {
+		// Write your test here and use APIs like `#expect(...)` to check expected conditions.
+		// Swift Testing Documentation
+		// https://developer.apple.com/documentation/testing
+	}
+	
+}
